a4883842cd7e0bd52e0fbf888197795ae7154d20
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2023-03-17 13:31:40 -0400

    Added Start and Stop methods

--- AutoDiscoveryLib/DiscoveryAgent.cs
+++ AutoDiscoveryLib/DiscoveryAgent.cs
@@ -1,12 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Crestron.SimplSharp;
+using Crestron.SimplSharp.CrestronSockets;
 
 namespace AutoDiscoveryLib
 {
     public class DiscoveryAgent
     {
+        private UDPServer _socket;
+
+        private bool _isActive;
+        public ushort Active
+        {
+            get
+            {
+                if (_isActive)
+                    return 1;
+                else
+                    return 0;
+            }
+        }
+
+        public DiscoveryAgent()
+        {
+            _socket = new UDPServer();
+        }
+
+        public void Start(string address, ushort port)
+        {
+            var result = _socket.EnableUDPServer(address, port);
+
+            if (result == SocketErrorCodes.SOCKET_OK)
+                _isActive = true;
+            else
+                _isActive = false;
+
+            CrestronConsole.PrintLine("Start result = {0}", result.ToString());
+        }
+
+        public void Stop()
+        {
+            if (_isActive)
+            {
+                var result = _socket.DisableUDPServer();
+
+                CrestronConsole.PrintLine("Stop result = {0}", result.ToString());
+            }
+
+            _isActive = false;
+        }
     }
 }
