fb7f28a45cfc67e5941129d3edfc6b179c2d07de
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2023-03-17 14:38:01 -0400

    Created DiscoveryPacket class

---
+++ AutoDiscoveryLib/DiscoveryPacket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+
+namespace AutoDiscoveryLib
+{
+    class DiscoveryPacket
+    {
+        public string ID { get; set; }
+        public string Address { get; set; }
+        public uint Clock { get; set; }
+
+        public DiscoveryPacket()
+        {
+        }
+    }
+}
