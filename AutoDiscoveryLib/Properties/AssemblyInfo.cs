5367dc8df4ea7ca95d07e4a8c7b798947c60a00c
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2023-03-17 12:14:16 -0400

    New SIMPL# library from template

---
+++ AutoDiscoveryLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("AutoDiscoveryLib")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AutoDiscoveryLib")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
