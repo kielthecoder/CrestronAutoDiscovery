de8f565304e62c31bb81006ba6685e7052de15a3
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2023-03-22 11:06:27 -0400

    Created DiscoveryAgent and DiscoveryPacket classes

---
+++ AutoDiscoveryLib4/DiscoveryAgent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoDiscoveryLib4
+{
+    public class DiscoveryAgent
+    {
+        private bool _active;
+        public ushort Active
+        {
+            get
+            {
+                // Translate bool to ushort for SIMPL+
+                if (_active)
+                    return 1;
+                else
+                    return 0;
+            }
+        }
+
+        public string IPv4 { get; set; }
+        public string Hostname { get; set; }
+        public string Description { get; set; }
+        public int PollingPeriod { get; set; }
+
+        public event EventHandler OnStarted;
+        public event EventHandler OnStopping;
+    }
+}
---
+++ AutoDiscoveryLib4/DiscoveryPacket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoDiscoveryLib4
+{
+    [Serializable]
+    internal class DiscoveryPacket
+    {
+        public int Version { get; set; }
+        public string IPv4 { get; set; }
+        public string Hostname { get; set; }
+        public string Description { get; set; }
+    }
+}
