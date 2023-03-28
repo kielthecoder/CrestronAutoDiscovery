5ba37d1309cd91b0e5530f64ff9ce99e9839fc7f
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2023-03-28 14:20:31 -0400

    ConsoleApp for testing

---
+++ DiscoveryConsoleApp/DiscoveryPacket.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiscoveryConsoleApp
+{
+    internal class DiscoveryPacket
+    {
+        public int Version { get; set; }
+        public string IPv4 { get; set; }
+        public string Hostname { get; set; }
+        public string Description { get; set; }
+    }
+}
